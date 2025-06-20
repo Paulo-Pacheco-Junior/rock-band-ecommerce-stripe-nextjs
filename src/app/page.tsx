76e27f3d55f9400a8d92d583653e151e56e59e03
Paulo-Pacheco-Junior <paulojrgyn88@gmail.com>
2025-06-20 10:41:16 -0300

    Incluir informação de pagamento por pix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
+import PaymentInfo from "@/components/PaymentInfo";
 import ProductCard from "../components/ProductCard";
 import { products } from "@/data/products";
 
 export default function Home() {
   return (
-    <main className="max-w-5xl mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-      {products.map((product, index) => (
-        <ProductCard
-          key={product.id}
-          product={product}
-          priority={index === 0}
-        />
-      ))}
-    </main>
+    <>
+      <main className="max-w-5xl mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mb-4">
+        {products.map((product, index) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            priority={index === 0}
+          />
+        ))}
+      </main>
+      <PaymentInfo />
+    </>
   );
 }
---
+++ src/components/PaymentInfo.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function PaymentInfo() {
+  return (
+    <div className="border border-gray-700 border-0_5 p-6 rounded shadow-sm max-w-xl mx-auto bg-gray-900 text-gray-100 mb-12">
+      <p className="mb-4">
+        - Envie o comprovante para{" "}
+        <a
+          href="https://w.app/r2jac7"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-400 hover:underline"
+        >
+          62996116534
+        </a>
+        .
+      </p>
+
+      <h2 className="text-lg font-semibold mb-2">
+        💲 <strong>PIX:</strong>
+      </h2>
+
+      <blockquote className="border-l-4 border-blue-500 pl-4 italic mb-4 text-gray-300">
+        <p>
+          <strong>Chave Aleatória:</strong> 341ac614-7167-416c-8054-2e39d34e73c9
+        </p>
+      </blockquote>
+
+      <p>Gabriel de Morais Pereira / NUBANK</p>
+
+      <p className="mt-4 italic text-sm text-gray-400">
+        Não se esqueça de nos enviar o comprovante! 😉
+      </p>
+    </div>
+  );
+}
